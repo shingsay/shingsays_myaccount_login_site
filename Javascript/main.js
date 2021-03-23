6dc072e541b84c3826aa2a2d2c9355eb15f04863
Shingsay Lee <shingsaylee@gmail.com>
2021-03-23 18:53:28 -0400

    most current update

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,9 +1,34 @@
 /** Show and Hide Hamburger Links **/
-function hamburgerClick() {
+
+/** function hamburgerClick() {
     var x = document.getElementById("hamburgerLinks");
     if (x.style.display === "block") {
      x.style.display = "none";
     } else {
-        x.style.display = "block";
+     x.style.display = "block";
     }
-}
+}  **/
+
+/** Hamburger Animation **/
+
+/* const hamburgerMenu = document.querySelector('.hamburgerMenu');
+let hamburgerOpen = false;
+hamburgerMenu.addEventListener('click', () => {
+    if(!hamburgerOpen) {
+        hamburgerMenu.classList.add('open');
+        hamburgerOpen = true;
+    }   else {
+        hamburgerMenu.classList.remove('open');
+        hamburgerOpen = false;
+    }
+}); */
+
+
+
+var nav = document.getElementById('access_nav'),
+    body = document.body;
+
+nav.addEventListener('click', function(e) {
+    body.className = body.className? '' : 'with_nav';
+    e.preventDefault();
+});
