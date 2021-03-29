a5ed8faa9f705c3c23b244329370841bf3700f8b
Shingsay Lee <shingsaylee@gmail.com>
2021-03-29 00:17:41 -0400

    commit readme file and updates

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,34 +1,30 @@
-/** Show and Hide Hamburger Links **/
-
-/** function hamburgerClick() {
-    var x = document.getElementById("hamburgerLinks");
-    if (x.style.display === "block") {
-     x.style.display = "none";
-    } else {
-     x.style.display = "block";
-    }
-}  **/
-
-/** Hamburger Animation **/
-
-/* const hamburgerMenu = document.querySelector('.hamburgerMenu');
-let hamburgerOpen = false;
-hamburgerMenu.addEventListener('click', () => {
-    if(!hamburgerOpen) {
-        hamburgerMenu.classList.add('open');
-        hamburgerOpen = true;
-    }   else {
-        hamburgerMenu.classList.remove('open');
-        hamburgerOpen = false;
-    }
-}); */
 
 
-
-var nav = document.getElementById('access_nav'),
+var nav = document.getElementById('openNav'),
     body = document.body;
 
 nav.addEventListener('click', function(e) {
     body.className = body.className? '' : 'with_nav';
     e.preventDefault();
 });
+
+/* Validate Email */
+
+function validate() {
+
+
+
+var email = document.getElementById('email').value;
+var password = document.getElementById('password').value;
+    if(email == "admin@example.com" && password == "password")
+    {
+        alert("Welcome admin@example.com!");
+        return false;
+    }
+    else 
+    {
+        alert("Please enter correct credentials");
+    }
+}
+
+
---
+++ README.md
@@ -0,0 +1,12 @@
+This project demonstrates my understanding of web design for Code Lousiville's Front End Website Development Course 2021. The concept is a simple account login for a website using the HTML Form.
+
+The following features were included in the project: 
+    1. A hamburger menu the opens and hides navigation links on the website. When the hamburger menu is clicked it opens the links formatted in a column. The menu bar also appears on the screen horizontally at 30% width at 768 pixels and full width at 996 pixels.
+
+    2. The hamburger menu has a slight animation when clicked.
+
+    3. The email and password inputs are validated and must have the correct credentials. If nothing is entered or the incorrect credentials are entered, a message will pop up saying, "Please enter correct credentials".
+        a. THE CREDENTIALS FOR THE FORM ARE: EMAIL: admin@example.com PASSWORD: password
+        b. Once the correct credentials are submitted a message will display a message saying, "Welcome admin@example.com"
+
+   
