283559c3006c86413d800b80d6cb1e59bb871555
Shingsay Lee <shingsaylee@gmail.com>
2021-03-31 17:59:56 -0400

    final updates

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,4 +1,4 @@
-
+/*** Open Hamburger ***/
 
 var nav = document.getElementById('openNav'),
     body = document.body;
@@ -8,11 +8,9 @@ nav.addEventListener('click', function(e) {
     e.preventDefault();
 });
 
-/* Validate Email */
+/*** Validate Email ***/
 
 function validate() {
-
-
 
 var email = document.getElementById('email').value;
 var password = document.getElementById('password').value;
@@ -25,6 +23,71 @@ var password = document.getElementById('password').value;
     {
         alert("Please enter correct credentials");
     }
+    
 }
 
+// Drag
 
+var dragItem = document.querySelector("#myAccountForm");
+var container = document.querySelector("#drag_container");
+
+var active = false;
+var currentX;
+var currentY;
+var initialX;
+var initialY;
+var xOffset = 0;
+var yOffset = 0;
+
+container.addEventListener("touchstart", dragStart, false);
+container.addEventListener("touchend", dragEnd, false);
+container.addEventListener("touchmove", drag, false);
+
+container.addEventListener("mousedown", dragStart, false);
+container.addEventListener("mouseup", dragEnd, false);
+container.addEventListener("mousemove", drag, false);
+
+function dragStart(e) {
+  if (e.type === "touchstart") {
+    initialX = e.touches[0].clientX - xOffset;
+    initialY = e.touches[0].clientY - yOffset;
+  } else {
+    initialX = e.clientX - xOffset;
+    initialY = e.clientY - yOffset;
+  }
+
+  if (e.target === dragItem) {
+    active = true;
+  }
+}
+
+function dragEnd(e) {
+  initialX = currentX;
+  initialY = currentY;
+
+  active = false;
+}
+
+function drag(e) {
+  if (active) {
+  
+    e.preventDefault();
+  
+    if (e.type === "touchmove") {
+      currentX = e.touches[0].clientX - initialX;
+      currentY = e.touches[0].clientY - initialY;
+    } else {
+      currentX = e.clientX - initialX;
+      currentY = e.clientY - initialY;
+    }
+
+    xOffset = currentX;
+    yOffset = currentY;
+
+    setTranslate(currentX, currentY, dragItem);
+  }
+}
+
+function setTranslate(xPos, yPos, el) {
+  el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+}
